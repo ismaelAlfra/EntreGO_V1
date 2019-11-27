b15a8fa93ed1972ca3a96f0c7b29cce1d13e452a
Erisson Nunes <42328316+bn-erisson@users.noreply.github.com>
2019-11-27 02:19:24 -0300

    chat

---
+++ Components/Chat.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import { ScrollView, View, TouchableOpacity, Text, Image, TextInput } from 'react-native';
+import EstilosEntreGO from './EstilosEntreGO';
+import MyButton from './library/MyButton';
+import Container from './library/Container';
+import Card from './library/Card';
+import Title from './library/Title';
+
+
+export default class Chat extends Component {
+
+    render() {
+        return (
+            <View style={EstilosEntreGO.fundo}>
+
+                <View style={{ width: 420, height: 800, marginTop: 25, justifyContent: "center", alignContent: "center", alignSelf: "center" }}>
+                    <Title>Chat - Ana, Pedro e Julia</Title>
+                    <Container >
+                        <ScrollView vertical={true} style={{ marginBottom: 30 }} >
+
+
+                            <View style={EstilosEntreGO.mensagem_enviada} cliente='Fulano' encomenda='Caneta Azul'>
+                                <Text style={{ fontFamily: 'Manjari-Bold' }}> Ana Claudia</Text>
+                                <Text style={EstilosEntreGO.texto_mensagem} >Tem como levar a encomenda no seu colo durante a viagem? Ela é um pouco frágil  </Text>
+                            </View>
+
+                            <View style={EstilosEntreGO.mensagem_respondida} cliente='Fulano' encomenda='Caneta Azul'>
+                                <Text style={{ fontFamily: 'Manjari-Bold' }} > Pedro Correia </Text>
+                                <Text style={EstilosEntreGO.texto_mensagem} >Eu também prefiro, tenho medo que quebre </Text>
+                            </View>
+
+                            <View style={EstilosEntreGO.mensagem_respondida} cliente='Fulano' encomenda='Caneta Azul'>
+                                <Text style={{ fontFamily: 'Manjari-Bold' }}> Julia Brandão </Text>
+                                <Text style={EstilosEntreGO.texto_mensagem} >Claro, posso levar sim, vamos fechar negócio então? </Text>
+                            </View>
+
+                        </ScrollView>
+
+                    </Container>
+
+                    <TextInput style={EstilosEntreGO.input_mensagem} placeholder={'Digite sua mensagem'} placeholderStyle={{ fontFamily: 'Manjari-Bold', borderColor: 'red' }} /* { height: 40, borderWidth: 0.5} onChangeText={text => onChangeText(text)} value={value} */ />
+                </View>
+            </View>
+        )
+    }
+}
